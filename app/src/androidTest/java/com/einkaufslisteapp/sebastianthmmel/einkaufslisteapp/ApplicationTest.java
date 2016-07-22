2f425fa0df14457bb37183c57d303342d10e6c16
sethuemm <sethuemm@htwg-konstanz.de>
2016-07-22 18:38:41 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/einkaufslisteapp/sebastianthmmel/einkaufslisteapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.einkaufslisteapp.sebastianthmmel.einkaufslisteapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
